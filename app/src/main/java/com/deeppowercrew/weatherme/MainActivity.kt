d3c0fc5ef302051e0b9f9c8b2174a6338d0c3ef9
DT5Gen <marinovod@gmail.com>
2022-09-09 06:02:27 +0300

    make test API

--- app/src/main/java/com/deeppowercrew/weatherme/MainActivity.kt
+++ app/src/main/java/com/deeppowercrew/weatherme/MainActivity.kt
@@ -2,6 +2,7 @@ package com.deeppowercrew.weatherme
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -11,9 +12,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.deeppowercrew.weatherme.screens.MainCard
 import com.deeppowercrew.weatherme.screens.TableLayout
 import com.deeppowercrew.weatherme.ui.theme.WeatherMeTheme
+
 const val API_KEY = "9c4dca2eee744d2f9ba134332220209"
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +26,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherMeTheme {
+                getWeatherData("Yokohama", this)
                 Image(
                     painter = painterResource(id = R.drawable.backgrnd),
                     contentDescription = "background",
@@ -39,6 +45,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun getWeatherData(city:String, context: Context){
+private fun getWeatherData(city: String, context: Context) {
+    val url =
+        "https://api.weatherapi.com/v1/forecast.json" + "?key=$API_KEY&" + "q=$city" + "&days=" +
+                "5" +
+                "&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url, { response ->
+            Log.d("MyLog", "Volley Error $response")
+        }, {
+                error -> Log.d("MyLog", "Volley Error $error")
 
+        }
+    )
+
+
+    queue.add(stringRequest)
 }
